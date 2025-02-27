f4d309c7e471bc04fbb0efeb34bbf0efc764f6f9
Nithin <58347462+nithinnnn@users.noreply.github.com>
2025-02-26 18:08:21 -0800

    0.1.2 - added Pinpoint dependencies, no more localization I guess

--- TeamCode/src/main/java/util/control/GoBildaPinpointDriver.java
+++ TeamCode/src/main/java/util/control/GoBildaPinpointDriver.java
@@ -20,7 +20,7 @@
  *   SOFTWARE.
  */
 
-package org.firstinspires.ftc.teamcode;
+package util.control;
 
 import static com.qualcomm.robotcore.util.TypeConversion.byteArrayToInt;
 
--- TeamCode/src/main/java/util/control/SensorGoBildaPinpointExample.java
+++ TeamCode/src/main/java/util/control/SensorGoBildaPinpointExample.java
@@ -20,7 +20,7 @@
  *   SOFTWARE.
  */
 
-package org.firstinspires.ftc.teamcode;
+package util.control;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
