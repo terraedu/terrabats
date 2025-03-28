149878388f3d868f16bd5e124811732053b2c36c
Nithin <58347462+nithinnnn@users.noreply.github.com>
2025-03-27 22:53:23 -0700

    ts not woring

--- TeamCode/src/main/java/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/FConstants.java
@@ -27,7 +27,7 @@ public class FConstants {
 
         FollowerConstants.forwardZeroPowerAcceleration = -16.7992911701766031;
         FollowerConstants.lateralZeroPowerAcceleration = -33.96098431447647;
-
+//1.
         FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.65,0,0.1,0);
         FollowerConstants.useSecondaryTranslationalPID = false;
 //        FollowerConstants.secondaryTranslationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0); // Not being used, @see useSecondaryTranslationalPID
